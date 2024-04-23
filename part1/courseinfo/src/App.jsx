30bb7e1e8b3f030ecd0e680ee9c26ec6ecb4961d
sujade <dnmzsu@gmail.com>
2024-04-23 12:32:38 +0200

    add part1 exercises 1.1-1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,66 @@
+const Header = ({course}) => {
+    return (
+        <>
+            {course.course}
+        </>
+    )
+}
+
+const Content = ({course}) => {
+    let parts = course["parts"]
+    const [part1, part2, part3] = parts
+
+    return (
+        <>
+            <Part part={part1.name} exercises={part1.exercises}/>
+            <Part part={part2.name} exercises={part2.exercises}/>
+            <Part part={part3.name} exercises={part3.exercises}/>
+        </>
+    )
+}
+
+const Part = ({part, exercises}) => {
+    return (
+        <>
+            {part} {exercises}
+        </>
+    )
+}
+
+const Total = ({course}) => {
+    let total = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+    return (
+        <>
+            <p>Number of exercises {total}</p>
+        </>
+    )
+}
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+    return (
+        <div>
+            <Header name={course}/>
+            <Content course={course}/>
+            <Total course={course}/>
+        </div>
+    )
+}
+
+export default App
